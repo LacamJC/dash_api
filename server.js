a5fc5d03e2b68f9f8b4db1c348fe3e7498358bde
LacamJC <joaoramajo744@gmail.com>
2025-03-19 17:41:04 -0300

    ADD AUTHENTICATION

--- server.js
+++ server.js
@@ -7,22 +7,36 @@ const morgan = require('morgan')
 const routes = require('./src/routes')
 const app = express()
 
+
+const chave = process.env.API_KEY
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(morgan("dev"))
-app.use("/dashboard", routes)
+app.use("/dashboard", authenticReq, routes)
 
-app.get("/", (req,res) => {
-    res.status(200).json({message:"Entre em contato com @LacamJC para mais informações"})
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Entre em contato com @LacamJC para mais informações" })
 })
+
+
+function authenticReq(req, res, next) {
+    const api_req = req.headers['token']
+
+    if (!api_req || api_req !== chave) {
+        return res.status(401).json({ message: "Acesso não autorizado" })
+    }
+
+    next()
+}
 
 
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(
-    `
+        `
     //////////////////
     
     Servidor aberto na porta: ${PORT}
